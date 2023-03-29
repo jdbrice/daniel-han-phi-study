46355df8c61102848583190eb45f71e513c9f726
Xihe Han <han.1929@osu.edu>
2023-03-29 14:00:02 -0400

    Initially Modified ana.C to add constraint on Pion

--- ana.C
+++ ana.C
@@ -1,6 +1,8 @@
 // A simple TTreeReader use: read data from hsimple.root (written by hsimple.C)
 #include "TFile.h"
 #include "TH1F.h"
+#include <TSystem.h>
+#include "TH2F.h"
 #include "TTreeReader.h"
 #include "TTreeReaderValue.h"
 #include "TLorentzVector.h"
@@ -28,6 +30,7 @@ void ana() {
     TH1F * hdca = new TH1F("dca", "Run19 Au+Au data;dca (cm); counts", 500, 0, 5);
     // Create a ROOT 1D histogram for parent Mass
     TH1F * mass = new TH1F("parent_mass", "Run19 Au+Au data;parent_mass (GeV); counts", 500, 0, 5);
+    TH2F * pt_mass = new TH2F("pt_mass",";mass(GeV);pt(GeV)", 5000,0,5, 500,0,5);
     // Open the file containing the tree (INPUT data).
     TFile *myFile = TFile::Open("input.root");
     //This setsup the reader, access the data
@@ -52,7 +55,8 @@ void ana() {
         hdca->Fill( dca2 );
 
 
-        if ( true ){ // event and track selection will go here
+//        if ( fabs(pair->d1_mNSigmaKaon) < 3. && fabs(pair->d2_mNSigmaKaon) < 3. && pair->d1_mPt > 0.01 && pair->d2_mPt > 0.01 ){ // event and track selection will go here
+        if(pair->mChargeSum == 0.){
             // Setup the Lorentz Vectors of the daugher tracks
             lv1.SetPtEtaPhiM( pair->d1_mPt, pair->d1_mEta, pair->d1_mPhi, 0.493 );
             lv2.SetPtEtaPhiM( pair->d2_mPt, pair->d2_mEta, pair->d2_mPhi, 0.493 );
@@ -64,21 +68,23 @@ void ana() {
             double px1 = cos(pair->d1_mPhi) * (pair->d1_mPt);
             double py1 = sin(pair->d1_mPhi) * (pair->d1_mPt);
             double pz1 = sinh(pair-> d1_mEta) * (pair-> d1_mPt);
-            double e1 = sqrt(pow(px1, 2.)+pow(py1, 2.)+pow(pz1,2.) + pow(0.493,2.));
+            double e1 = sqrt(pow(px1, 2.)+pow(py1, 2.)+pow(pz1,2.) + pow(0.139,2.));
             // compute the four-momentum vector for particle 2
             double px2 = cos(pair->d2_mPhi) * (pair->d2_mPt);
             double py2 = sin(pair->d2_mPhi) * (pair->d2_mPt);
             double pz2 = sinh(pair-> d2_mEta) * (pair-> d2_mPt);
-            double e2 = sqrt(pow(px2, 2.)+pow(py2, 2.)+pow(pz2,2.) + pow(0.493,2.));
+            double e2 = sqrt(pow(px2, 2.)+pow(py2, 2.)+pow(pz2,2.) + pow(0.139,2.));
 
             // use conservation of momentum and energy to set the four-momentum
             // vector for parent particle.
             lv.SetPxPyPzE(px1+px2, py1 + py2, pz1 + pz2, e1 + e2);
-            // extract the parent mass from the parent four vector
+            lv = lv1 + lv2;// extract the parent mass from the parent four vector
             parent_mass = lv.E() - fabs(lv.Px()) - fabs(lv.Py()) - fabs(lv.Pz());
             parent_mass = sqrt(pow(lv.E(),2) - pow(lv.Px(), 2.)+pow(lv.Py(), 2.)+pow(lv.Pz(),2.));
 
             mass->Fill( parent_mass );
+
+            pt_mass -> Fill(lv.M(), lv.Pt());
         } // selection
     } // loop on events
 
@@ -93,7 +99,9 @@ void ana() {
 
     // save plot to a PDF
     gPad->Print( "parent_mass_log.pdf" );
-
+    pt_mass-> Draw("colz");
+    gPad->SetLogy(0); // set semilog-y
+    gPad->SetLogz(1); // set semilog-z
     // write all histograms to output data file
     fo->Write();
   }             
