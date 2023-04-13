8d27ccbb16b2e70e643e7d35d663ff8bd1a427ad
Xihe Han <han.1929@osu.edu>
2023-04-13 02:36:47 -0400

    First attempt at doing Kaon selection

---
+++ phi_selection/daughter_distribution.C
@@ -0,0 +1,105 @@
+// A simple TTreeReader use: read data from hsimple.root (written by hsimple.C)
+#include "TFile.h"
+#include "TH1F.h"
+#include <TSystem.h>
+#include "TH2F.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "TLorentzVector.h"
+#include "TCanvas.h"
+#include "TH3F.h"
+#include "TColor.h"
+#include <cmath>
+#include <math.h>
+#include "FemtoPairFormat.h"
+
+// ClassImp( FemtoPair );
+
+int ican = 0;
+void makeCan() {
+    TCanvas * can = new TCanvas( TString::Format( "can%d", ican++ ), "", 900, 600 );
+    can->SetTopMargin(0.06);
+    can->SetRightMargin(0.01);
+}
+
+void daughter_distribution() {
+    //    Create a file for output (saving) of histograms we compute
+    TFile * fo = new TFile( "inpuit.root", "RECREATE" );
+
+    // Create a ROOT 1D histogram
+    TH1F * hdca = new TH1F("dca", "Run19 Au+Au data;dca (cm); counts", 500, 0, 5);
+    // Create a ROOT 1D histogram for parent Mass
+    TH1F * mass = new TH1F("parent_mass", "Run19 Au+Au data;parent_mass (GeV); counts", 500, 0, 5);
+    TH2F * pt_mass = new TH2F("pt_mass",";mass(GeV);pt(GeV)", 5000,0,5, 500,0,5);
+
+    // Create a ROOT 2D histogram for Pion/Kaon distributions
+    TH2F * track1_sigma_KP= new TH2F("Track1",";d1_N_sigma_kaon;d1_N_sigma_pion", 500, -5 , 5, 500, -5, 5);
+    TH2F * track2_sigma_KP= new TH2F("Track2",";d2_N_sigma_kaon;d2_N_sigma_pion", 500, -5 , 5, 500, -5, 5);
+    TH1F * phi_pt = new TH1F("phi_pt", ";Phi_pt(GeV); Count", 500, 0, 2.);
+    // Open the file containing the tree (INPUT data).
+    TFile *myFile = TFile::Open("input.root");
+    //This setsup the reader, access the data
+    TTreeReader myReader("PairDst", myFile);
+
+    // We need this so ROOT understands the data format
+    gSystem->Load("FemtoPairFormat_h.so");
+    TTreeReaderValue<FemtoPair> pair(myReader, "Pairs");
+
+    // Lorentz vectors (4-vectors) for kinematics in special relativity
+    TLorentzVector lv1, lv2, lv;
+
+    double parent_mass;
+    // Loop over all entries of the TTree or TChain.
+    while (myReader.Next()) {
+        // get their distance to closest approach ( info about track pair)
+        double parent_mass = 0;
+        double dca1 = pair->d1_mDCA;
+        double dca2 = pair->d2_mDCA;
+
+        hdca->Fill( dca1 );
+        hdca->Fill( dca2 );
+
+
+//        if ( fabs(pair->d1_mNSigmaKaon) < 3. && fabs(pair->d2_mNSigmaKaon) < 3. && pair->d1_mPt > 0.01 && pair->d2_mPt > 0.01 ){ // event and track selection will go here
+
+
+//      only select += pair
+        if(pair->mChargeSum == 0.){
+            // Setup the Lorentz Vectors of the daugher tracks
+            lv1.SetPtEtaPhiM( pair->d1_mPt, pair->d1_mEta, pair->d1_mPhi, 0.493 ); // we use Kaon mass here.
+            lv2.SetPtEtaPhiM( pair->d2_mPt, pair->d2_mEta, pair->d2_mPhi, 0.493 );
+            // compute parent particle lorentz vector from daughters
+
+            // use conservation of momentum and energy to set the four-momentum
+            // vector for parent particle.
+            lv = lv1 + lv2;
+
+            // extract the parent mass from the parent four vector
+            parent_mass = lv.M();
+            pt_mass -> Fill(lv.M(), lv.Pt());
+
+            track1_sigma_KP -> Fill(pair->d1_mNSigmaKaon, pair->d1_mNSigmaPion);
+            track2_sigma_KP -> Fill(pair->d2_mNSigmaKaon, pair->d2_mNSigmaPion);
+
+
+            // We apply the selection rule to select Kaon, and fill the phi_pt values for phi mass += 15MeV 
+            if(fabs(pair->d1_mNSigmaKaon) < .5 && fabs(pair->d2_mNSigmaKaon) < .5 &&lv.M()>= 1.019+ 0.015 && pair->d1_mPt > 0.01 && pair->d2_mPt > 0.01){
+                phi_pt-> Fill(lv.Pt());
+            }
+        } // selection
+    } // loop on events
+
+    fo->cd();
+
+    makeCan();
+
+    // pt_mass-> Draw("colz");
+    
+    // track1_sigma_KP-> Draw("colz");
+    // track2_sigma_KP-> Draw("colz");
+
+    phi_pt->Draw();
+
+    // write all histograms to output data file
+    fo->Write();
+  }             
