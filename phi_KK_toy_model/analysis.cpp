96c926e38a6056aa963083c3f985e1c40e57d442
Xihe Han <han.1929@osu.edu>
2023-05-04 16:13:11 -0400

    temp moved rountines into main. First reconstruction success

--- phi_KK_toy_model/analysis.cpp
+++ phi_KK_toy_model/analysis.cpp
@@ -1,16 +1,35 @@
-#include "Random_routines.h"
-#include <TLorentzVector.h>
-#include <TROOT.h>
-#include <cmath>
+#include <fstream>
 #include <iostream>
+#include <TH1F.h>
+#include <TCanvas.h>
+#include <TLorentzVector.h>
+#include <cmath>
+#include <TRandom3.h>
 
+double PHI_MASS = 1.019;
+double KAON_MASS = 0.493;
+double PT_MIN = 0.;
+double PT_MAX = 2.;
+double ETA_MIN = 0.;
+double ETA_MAX = 0.;
+double PHI_MIN = 0.; 
+double PHI_MAX = 2 * M_PI;
+double SAMPLE_SIZE = 2e6;
 double pi = M_PI;
 thread_local TRandom3 rng(0);
+
+int ican = 0;
+void makeCan() {
+    TCanvas * can = new TCanvas( TString::Format( "can%d", ican++ ), "", 900, 600 );
+    can->SetTopMargin(0.06);
+    can->SetRightMargin(0.01);
+}
+
 
 // return a randomly parepared particle lorentz vector ptr with known mass
 // and bounded pt-> transverse momentum, eta-> pseudorapidity, phi-> transverse
 // azimuthal angle
-TLorentzVector *Random_routines::get_random_lorentz_vector(
+TLorentzVector * get_random_lorentz_vector(
     double pt_min, double pt_max, double eta_min, double eta_max,
     double phi_min, double phi_max, double mass) {
   TLorentzVector *random_vector_ptr = new TLorentzVector;
@@ -31,7 +50,7 @@ TLorentzVector *Random_routines::get_random_lorentz_vector(
 // contained in the vector.
 
 std::vector<TLorentzVector *>
-Random_routines::symmetrical_two_body_decay(TLorentzVector *parent_particle,
+symmetrical_two_body_decay(TLorentzVector *parent_particle,
                                             double daughter_mass) {
   TLorentzVector *daughter_one = new TLorentzVector;
   TLorentzVector *daughter_two = new TLorentzVector;
@@ -80,3 +99,35 @@ Random_routines::symmetrical_two_body_decay(TLorentzVector *parent_particle,
   return daughter_list;
 }
 
+
+
+void analysis()
+{
+    std::vector<TLorentzVector*> parent_vector;
+    std::vector<TLorentzVector*> daughter1_vector;
+    std::vector<TLorentzVector*> daughter2_vector;
+
+    for (int i = 0; i < SAMPLE_SIZE; i++)
+    {
+        TLorentzVector* parent_particle_ptr =  get_random_lorentz_vector(PT_MIN, PT_MAX, ETA_MIN, ETA_MAX, PHI_MIN, PHI_MAX, PHI_MASS);
+        parent_vector.push_back(parent_particle_ptr);
+        std::vector<TLorentzVector*> daughter_ptr_pair =  symmetrical_two_body_decay(parent_particle_ptr, KAON_MASS);
+        daughter1_vector.push_back(daughter_ptr_pair[0]);
+        daughter2_vector.push_back(daughter_ptr_pair[1]);
+
+    }
+                        
+    TH1F* combined_masses = new TH1F("Combined Masses", "Toy Model Combined Masses;m_{K^+ K^-}(GeV);count", 500, 1.0, 1.1);
+    for (int i = 0; i < SAMPLE_SIZE; i++)
+    {
+       TLorentzVector reconstructed_parent_vector = *daughter1_vector[i] + *daughter2_vector[i]; 
+        
+       if(daughter1_vector[i]->Pt() > 0.06 && daughter2_vector[i]->Pt() > 0.06){
+            combined_masses->Fill(reconstructed_parent_vector.M());
+        }
+    }
+
+
+    makeCan();
+    combined_masses->Draw();
+}
