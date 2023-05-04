79de8343f248e36301f8ffd11b106a8e921f89da
Xihe Han <han.1929@osu.edu>
2023-05-04 14:53:53 -0400

    finished decay and random parent generate code

--- phi_KK_toy_model/Random_routines.cpp
+++ phi_KK_toy_model/Random_routines.cpp
@@ -1,74 +1,91 @@
 #include "Random_routines.h"
 #include <TLorentzVector.h>
+#include <TROOT.h>
+#include <cmath>
 #include <iostream>
-#include <random>
 
-// random number generator seed
-// This is used when the user enter 0 as seed
-using std::endl;
+double pi = M_PI;
+thread_local TRandom3 rng(0);
 
-std::random_device rd;
-std::mt19937_64 global_rng(rd());
+// return a randomly parepared particle lorentz vector ptr with known mass
+// and bounded pt-> transverse momentum, eta-> pseudorapidity, phi-> transverse
+// azimuthal angle
+TLorentzVector *Random_routines::get_random_lorentz_vector(
+    double pt_min, double pt_max, double eta_min, double eta_max,
+    double phi_min, double phi_max, double mass) {
+  TLorentzVector *random_vector_ptr = new TLorentzVector;
+  // get random pt from given bounds
+  double pt = rng.Uniform(pt_min, pt_max);
+  // get random eta from given bounds
+  double eta = rng.Uniform(eta_min, eta_max);
+  // get random phi from given bounds
+  double phi = rng.Uniform(phi_min, phi_max);
 
-// function definitions see header file "Random_routines.h"
-int Random_routines::get_random_int_std(int lower_bound, int upper_bound,
-                                        unsigned long int seed) {
-  std::uniform_int_distribution<int> dist(lower_bound, upper_bound + 1);
-  if (seed == 0) {
-    return dist(global_rng);
-  }
-  std::mt19937_64 local_rng(seed);
-  return dist(local_rng);
+  random_vector_ptr->SetPtEtaPhiM(pt, eta, phi, mass);
+
+  return random_vector_ptr;
 }
 
-double Random_routines::get_random_double_std(double lower_bound,
-                                              double upper_bound,
-                                              unsigned long int seed) {
-  std::uniform_real_distribution<double> dist(lower_bound, upper_bound);
-  if (seed == 0) {
-    return dist(global_rng);
-  }
-  std::mt19937_64 local_rng(seed);
-  return dist(local_rng);
+// return a vector that contain the equal mass two-particle decay process from a
+// given parent particle lorentz vector. The address of both dauthers are
+// contained in the vector.
+
+std::vector<TLorentzVector *>
+Random_routines::symmetrical_two_body_decay(TLorentzVector *parent_particle,
+                                            double daughter_mass) {
+  TLorentzVector *daughter_one = new TLorentzVector;
+  TLorentzVector *daughter_two = new TLorentzVector;
+  std::vector<TLorentzVector *> daughter_list;
+  daughter_list.push_back(daughter_one);
+  daughter_list.push_back(daughter_two);
+
+  // randomly generate polar angle for particle oen
+  double kaon1_polar = rng.Uniform(0., pi);
+  // randomly generate azimuthal angle for particle one
+  double kaon1_azimuth = rng.Uniform(0., 2. * pi);
+
+  // calculate the rest frame momentum for one particle using
+  // special relativity. Note that this calculation is true due to the
+  // conservation of momentum in the rest frame of the parent particle.
+  double kaon1_momentum =
+      std::sqrt((parent_particle->M() * parent_particle->M() -
+                 4 * daughter_mass * daughter_mass) /
+                4.);
+
+  // calculate the four vector of the first daughter particle
+  double kaon1_px =
+      kaon1_momentum * std::sin(kaon1_polar) * std::cos(kaon1_azimuth);
+
+  double kaon1_py =
+      kaon1_momentum * std::sin(kaon1_polar) * std::sin(kaon1_azimuth);
+
+  double kaon1_pz = kaon1_momentum * std::cos(kaon1_polar);
+
+  double kaon1_E =
+      std::sqrt(daughter_mass * daughter_mass + kaon1_px * kaon1_px +
+                kaon1_py * kaon1_py + kaon1_pz * kaon1_pz);
+
+  daughter_one->SetPxPyPzE(kaon1_px, kaon1_py, kaon1_pz, kaon1_E);
+  // due to momentum conservation, the second daughter particle will have the
+  // exact opposite signed momentum compared with the first daughter particle
+  // with the same amount of energy.
+  daughter_two->SetPxPyPzE(-kaon1_px, -kaon1_py, -kaon1_pz, kaon1_E);
+
+  // we boost the daughter particles to the lab frame, where the parent particle
+  // is moving
+  TVector3 boost_vector = parent_particle->BoostVector();
+  daughter_one->Boost(boost_vector);
+  daughter_two->Boost(boost_vector);
+
+  return daughter_list;
 }
 
-double Random_routines::get_random_gaussian_std(double mean, double stdev,
-                                                unsigned long seed) {
-  std::normal_distribution<double> dist(mean, stdev);
-  if (seed == 0) {
-    return dist(global_rng);
-  }
-  std::mt19937_64 local_rng(seed);
-  return dist(local_rng);
-}
+// TEST MAIN FUNCTION- TO BE DELETED
+int main() {
+  TLorentzVector *phi = Random_routines::get_random_lorentz_vector(
+      0., 10., 0., 4., 0., 2. * pi, 1.019);
 
-double Random_routines::get_random_flat_std(double lower_bound,
-                                            double upper_bound,
-                                            unsigned long seed) {
-  std::uniform_real_distribution<double> dist(lower_bound, upper_bound);
-  if (seed == 0) {
-    return dist(global_rng);
-  }
-  std::mt19937_64 local_rng(seed);
-  return dist(local_rng);
-}
+  std::vector<TLorentzVector *> daughter_pair =
+      Random_routines::symmetrical_two_body_decay(phi, 0.493);
 
-TLorentzVector *Random_routines::get_uniform_randomized_lorentz_ptr(
-    double mass, double Pt_min, double Pt_max, double Peta_min, double Peta_max,
-    double phi_min, double phi_max, unsigned long seed) {
-
-  if (seed == 0) {
-    double pt = get_random_flat_std(Pt_min, Pt_max, 0);
-    double peta = get_random_flat_std(Peta_min, Peta_max, 0);
-    double phi = get_random_flat_std(phi_min, phi_max, 0);
-    TLorentzVector *lorentz_vector_ptr = new TLorentzVector();
-    lorentz_vector_ptr->SetPtEtaPhiM(pt, peta, phi, mass);
-    return lorentz_vector_ptr;
-  }
-  double pt = get_random_flat_std(Pt_min, Pt_max, seed);
-  double peta = get_random_flat_std(Peta_min, Peta_max, seed);
-  double phi = get_random_flat_std(phi_min, phi_max, seed);
-  TLorentzVector *lorentz_vector_ptr = new TLorentzVector();
-  lorentz_vector_ptr->SetPtEtaPhiM(pt, peta, phi, mass);
-  return lorentz_vector_ptr;
 }
--- phi_KK_toy_model/Random_routines.h
+++ phi_KK_toy_model/Random_routines.h
@@ -1,62 +1,26 @@
-// Wrapper for random routines from :
-//	1. ROOT
-//	2. MT10037
-//	3. GSL
-//
-// Programmer: Xihe Han
-//
-// Changelog:
-//	- 4/26/2023 initial commit
-//////////////////////////////////////
+#include "TLorentzVector.h"
+#include "TRandom3.h"
+#include <vector>
 
-// define namespace
 #ifndef RANDOM_ROUTINES
+// define namespace
 #define RANDOM_ROUTINES
-
-// cpp built in random lib
-#include <random>
-// ROOT MT19937 implementation
-#include <TRandom3.h>
-// ROOT lorentz vector
-#include <TLorentzVector.h>
-// GSL random lib
-#include <gsl/gsl_rng.h>
-// GSL random distribution
-#include <gsl/gsl_randist.h>
-
-// function prototypes
 namespace Random_routines {
-// the following routines requires a seed to be passed in for random number generation 
-// if you don't care about seeding, please use 0 for the seed. If so, a random seed will 
-// be generated 
 
-// return random integer in the range [lower_bound, upper_bound]
-int get_random_int_std(int lower_bound, int upper_bound, unsigned long int seed);
-int get_random_int_root(int lower_bound, int upper_bound, unsigned long int seed);
-int get_random_int_gsl(int lower_bound, int upper_bound, unsigned long int seed);
-
-// return random double in the range [lower_bound, upper_bound]
-double get_random_double_std(double lower_bound, double upper_bound, unsigned long int seed);
-double get_random_double_root(double lower_bound, double upper_bound, unsigned long int seed);
-double get_random_double_gsl(double lower_bound, double upper_bound, unsigned long int seed);
-
-// return random double based on Gaussian distribution
-// with mu = mean, sigma = stdev
-double get_random_gaussian_std(double mean, double stdev, unsigned long int seed);
-double get_random_gaussian_root(double mean, double stdev, unsigned long int seed);
-double get_random_gaussian_gsl(double mean, double stdev, unsigned long int seed);
-
-// return random double based on flat distribution
-// in the range [min, max]
-double get_random_flat_std(double min, double max, unsigned long int seed);
-double get_random_flat_root(double min, double max, unsigned long int seed);
-double get_random_flat_gsl(double min, double max, unsigned long int seed);
-
-// return a Lorentz vector ptr with a fixed mass, random transverse momentum
-// Pt with a range, random Peta with a range, random phi with a range
-TLorentzVector *get_uniform_randomized_lorentz_ptr(double mass, double Pt_min,
-                                           double Pt_max, double Peta_min,
-                                           double Peta_max, double phi_min,
-                                           double phi_max, unsigned long int seed);
+// return a Lorentz vector with randomized four-vectors with
+// bounded Pt, eta, phi and a known mass in GeV
+TLorentzVector *get_random_lorentz_vector(double Pt_min, double Pt_max,
+                                          double eta_min, double eta_max,
+                                          double phi_min, double phi_max,
+                                          double mass);
+std::vector<TLorentzVector *>
+symmetrical_two_body_decay(TLorentzVector *parent_particle,
+                           double daughter_mass);
 }; // namespace Random_routines
-#endif
+
+// simulation random decay process from a parent particle with known mass to two
+// identical daughte particles with known mass.
+//
+// Funtion return a list of lorentz vectors with the pointer of the two dauthers
+
+#endif // !RANDOM_ROUTINES
