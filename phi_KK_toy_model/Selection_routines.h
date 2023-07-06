1bd2224149b0e0d8f77e24806c575db10bba1dce
Xihe Han <han.1929@osu.edu>
2023-07-06 18:39:15 -0400

    initial fix dEdx

--- phi_KK_toy_model/Selection_routines.h
+++ phi_KK_toy_model/Selection_routines.h
@@ -12,8 +12,7 @@
 // sigma away of that four momentum to either Pion or Kaon
 // using dE/dx vs Pt with a known blur(sigma). The "true" dE/dx file
 // must be provided as a root file for this code to function.
-class Selector
-{
+class Selector {
 
 public:
   // constructor for the particle selector. This requires the absolute file path
@@ -25,42 +24,38 @@ public:
   // destructor
   ~Selector(){};
 
-  // calculate NSigmaKaon with a parent vector and its topological
-  // reconstruction. The target is the exact dEdx of Kaon
-  // , while the "measurement" is the blurred dEdx of the assumed particle
-  double get_NSigmaKaon(TLorentzVector *mc_ptr);
-
-  // calculate NSigmaPion with a parent vector and its topological
-  // reconstruction. The target is the exact dEdx of Pion
-  // , while the "measurement" is the blurred dEdx of the assumed particle
-  double get_NSigmaPion(TLorentzVector *mc_ptr);
-
-  double get_NSigmaElectron(TLorentzVector *mc_ptr);
-
   // draw the blurred version of dEdx for both particles
   void draw_dEdx();
-
-  // draw dEdx for both particles
-  void draw_dEdx_blurred();
-
-private:
-  // fixed blur. This is obtained by adding a gaussian blur of dEdx using
-  // the precise method of "eyeballing" to match the dEdx measurement graph
-  double sigma = 0.00025;
-
   // path to true dE/dx root file
   // It is assumed that the true dEdx file is named "dEdx.root"
   // in the directory where the analysis code is compiled.
-  std::string file_path = "dEdx.root";
 
   // Root object for the dEdx file
-  TFile *dEdx_file = new TFile(file_path.c_str());
+  TFile *dEdx_file = new TFile("dEdx.root");
 
   // Root histogram for dEdxKaon using most probable value
   TH1D *dEdxKaon = (TH1D *)dEdx_file->Get("mpmK");
   // Root histogram for dEdxPion using most probable value
   TH1D *dEdxPion = (TH1D *)dEdx_file->Get("mpmPi");
   TH1D *dEdxElectron = (TH1D *)dEdx_file->Get("mpmE");
+
+  // calculate the nsigma values
+  double compute_NSigmaPion(double pt, TH1 *particle_dedx_distr);
+  double compute_NSigmaKaon(double pt, TH1 *particle_dedx_distr);
+  double compute_NSigmaElectron(double pt, TH1 *particle_dedx_distr);
+
+  // draw dEdx for both particles
+  void draw_dEdx_blurred();
+
+private:
+  // return the smeared dedx value for a given particle type;
+  double sample_dedx(double pt, TH1 *particle_dedx_distr);
+
+private:
+  // fixed blur. This is obtained by adding a gaussian blur of dEdx using
+  // the precise method of "eyeballing" to match the dEdx measurement graph
+  double sigma = 0.00025;
+
 };
 
 #endif
